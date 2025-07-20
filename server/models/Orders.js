868fce5c05e56e496723cd856af04df738385691
rahulgrover-rg <rahulgrover9373@gmail.com>
2025-07-20 17:05:18 +0530

    Database Definition - Models

---
+++ server/models/Orders.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const OrdersSchema = new mongoose.Schema({
+    products: {
+        type: [{
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Products",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+        }],
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    total_amount: {
+        type: mongoose.Types.Decimal128,
+        required: true,
+    },
+    address: {
+        type: String,
+        default: "",
+    },
+    status: {
+        type: String,
+        default: "Payment Done"
+    },
+}, {timestamps: true}); 
+
+export default mongoose.model("Shopping-Orders", OrdersSchema);
